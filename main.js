b13998a3e664135e23598cfe247e0457ead067a3
Pablo GP <pablogp@S407009.local>
2025-12-28 23:37:38 +0100

    code challenge L2

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Socks',
+            description: 'Most beautifull socks in the world!'
+        }
+    }
+}) 
